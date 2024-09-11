aac68ec41425aed4820803af5b15673bb28795c4
EduardoBalderas <164437245+EduardoBalderas@users.noreply.github.com>
2024-09-11 12:04:30 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//reset animations
+setInterval(()=>{
+    let el = document.getElementById('love')
+    var newone = el.cloneNode(true);
+    el.parentNode.replaceChild(newone, el);
+  }, 4000)
+  
